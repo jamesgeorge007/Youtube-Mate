410440e1da6eb66d8f2ace97a080791ce284b4b6
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-11-25 15:54:44 +0530

    minor fix :+1:

--- src/mixins/convert.js
+++ src/mixins/convert.js
@@ -1,10 +1,12 @@
 let convertBtn = document.querySelector('#convertBtn');
 let videoId = document.querySelector('#videoId').value;
+const axios = require('axios');
 
 // Online service URL
 const API_URL = `https://www.easy-youtube-mp3.com/download.php?v=${videoId}`; 
 
 // Event-listener for the convert button
 convertBtn.addEventListener('click', () => {
-    axios.get(`${API_URL}`);    
+    axios.get(`${API_URL}`);
+    // Handle the promise
 });
