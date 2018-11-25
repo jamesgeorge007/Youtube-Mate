f76441a689dc4ae60bac4fc47c4ee02af30365a9
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-11-25 15:48:48 +0530

    query online service :+1:

--- src/mixins/convert.js
+++ src/mixins/convert.js
@@ -1,4 +1,10 @@
 let convertBtn = document.querySelector('#convertBtn');
+let videoId = document.querySelector('#videoId').value;
+
+// Online service URL
+const API_URL = `https://www.easy-youtube-mp3.com/download.php?v=${videoId}`; 
+
+// Event-listener for the convert button
 convertBtn.addEventListener('click', () => {
-    alert(`${convertBtn.innerHTML} selected!`);
+    axios.get(`${API_URL}`);    
 });
